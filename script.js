4f1c3afa4ec348a0d1f593ec2389be308878608b
Christine Doan <angelstudyblr@gmail.com>
2025-02-27 20:26:37 -0800

    the display it not workingggggg

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const display = document.getElementById("result");
 
 function updateDisplay(input){
+    if (display.value === "0") {
+        display.value = input;
+    } else {
     display.value += input;
+    }
 }
+
 
 function clearDisplay(){
     display.value = "";
